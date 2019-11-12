8915c07a49da007794daa4c4e60892fdc82ebe4e
aalevelaa <topiaceo@gmail.com>
2019-11-12 21:01:39 +0100

    PHP first steps

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+
+	<head>
+		<title>Hospital General</title>
+	</head>
+
+	<body>
+
+		<header>
+			<h2>
+				HOSPITAL GENERAL
+			</h2>
+			<h4>
+				ACCESO AL PORTAL
+			</h4>
+		</header>
+
+		<div>
+			<form action="login.php" method="post">
+				<input type="submit" name="user" />
+				<input type="submit" name="manager" />
+			</form>
+		</div>
+
+	</body>
+
+</html>
